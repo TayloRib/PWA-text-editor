065c282231f2e3888d2c287b480c6bcc9b16b588
Ribbit <taylor.orsini@yahoo.com>
2023-05-25 21:34:56 -0700

    pushing to repo

---
+++ client/src-sw.js
@@ -0,0 +1,45 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const CACHE_NAME = 'JATE-cache'
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      '/database.js',
+      '/editor.js',
+      '/header.js',
+      '/index.js',
+      '/install.js',
+      '/style.css'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          const fetchResponse = await fetch(event.request);
+
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+
+        }
+    }
+  })());
+});
